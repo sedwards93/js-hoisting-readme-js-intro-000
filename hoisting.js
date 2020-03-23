ef05d3161004328ad2b0244d5dcb8f58144c14ec
Symion Edwards <symion.edwards@gmail.com>
2020-03-23 10:22:26 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -22,10 +22,9 @@ function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-  sayMy();
-
   function sayMy() {
     console.log(name);
     var name = "Kristin";
   }
+  sayMy();
 }
