c26d2285807622e8bc7834969250fb393f201bb4
Symion Edwards <symion.edwards@gmail.com>
2020-03-23 10:21:02 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,11 +1,11 @@
 function callMe() {
-  var lyric = "maybe";
+  
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
+  var lyric = "maybe";
   return lyric;
-
 }
 
 
@@ -21,11 +21,11 @@ function crazy() {
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
+
+  sayMy();
+
   function sayMy() {
     console.log(name);
     var name = "Kristin";
-  sayMy();
-
-
   }
 }
